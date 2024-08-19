a13eead73611ba37c772faff124d18bb885f5076
wenyan <wenyan@go-goal.com>
2024-08-19 10:26:30 +0800

    feat: diff

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -2,21 +2,21 @@ import { reactive } from "@vue/reactivity"
 import { hasOwn, isFunction, isObject, isString, ShapeFlags } from "@vue/shared"
 import { ReactiveEffect } from "packages/reactivity/src/effect"
 import { createComponentInstance } from "."
-import { createVnode,isSameVnode,Text } from "./vnode"
+import { createVnode, isSameVnode, Text } from "./vnode"
 
-function getSequence(arr){
+function getSequence(arr) {
   let len = arr.length
-  const result  = [0] // 这里方的是索引
+  const result = [0] // 这里方的是索引
   let p = arr.slice(0)
   let lastIndex
   let start
   let end
   let middle
-  for(let i = 0; i < len; i++){
+  for (let i = 0; i < len; i++) {
     const arrI = arr[i]
-    if(arrI !== 0){
+    if (arrI !== 0) {
       lastIndex = result[result.length - 1]
-      if(arr[lastIndex] < arrI){
+      if (arr[lastIndex] < arrI) {
         // 记录当年前前一个人索引
         p[i] = lastIndex
         result.push(i)
@@ -24,18 +24,18 @@ function getSequence(arr){
       }
       // 二分查找替换元素
       start = 0;
-      end = result.length -1
+      end = result.length - 1
       while (start < end) {
         middle = Math.floor((start + end) / 2)
         // 找到序列中间的值，通过索引找到对应的值
-        if(arr[result[middle]] < arrI){
+        if (arr[result[middle]] < arrI) {
           start = middle + 1
-        }else {
+        } else {
           end = middle
         }
       }
-      if(arrI < arr[result[start]]){
-        p[i] = result[start -1] //替换索引
+      if (arrI < arr[result[start]]) {
+        p[i] = result[start - 1] //替换索引
         result[start] = i   // 找到更有潜力的替换之前的（贪心算法）但这里[0,1,4,3]结果是错误的
       }
     }
@@ -48,16 +48,16 @@ function getSequence(arr){
   }
   return result
 }
-export function initProps(instance,rawProps) {
+export function initProps(instance, rawProps) {
   const props = {}
   const attrs = {}
   const options = Object.keys(instance.propsOptions) //用户注册过的props
-  if(rawProps) {
-    for(let key in rawProps){
+  if (rawProps) {
+    for (let key in rawProps) {
       const value = rawProps[key]
-      if(options.includes(key)) {
+      if (options.includes(key)) {
         props[key] = value
-      }else {
+      } else {
         attrs[key] = value
       }
     }
@@ -67,53 +67,53 @@ export function initProps(instance,rawProps) {
 }
 export function createSetupContext(instance) {
   return {
-    attrs:instance.attrs,
-    slots:instance.slots,
-    emit:instance.emit,
-    expose:(exposed)=>instance.exposed = exposed || {}
+    attrs: instance.attrs,
+    slots: instance.slots,
+    emit: instance.emit,
+    expose: (exposed) => instance.exposed = exposed || {}
   }
 }
 export function setupStatefullComponent(instance) {
   // 核心就是调用组件的setup方法
   const Component = instance.type
   const { setup } = Component
-  instance.proxy = new Proxy(instance.ctx,{
-    get({_:instance}, key, receiver) {
-      const { setupState,props} = instance
-      if(hasOwn(setupState,key)) {
+  instance.proxy = new Proxy(instance.ctx, {
+    get({ _: instance }, key, receiver) {
+      const { setupState, props } = instance
+      if (hasOwn(setupState, key)) {
         return setupState[key]
-      }else if(hasOwn(props,key)) {
+      } else if (hasOwn(props, key)) {
         return props[key]
       }
     },
-    set({_:instance}, key,value, receiver){
-      const {setupState,props} = instance   //props不可以修改
-      if(hasOwn(setupState,key)) {
+    set({ _: instance }, key, value, receiver) {
+      const { setupState, props } = instance   //props不可以修改
+      if (hasOwn(setupState, key)) {
         return setupState[key] = value
-      }else if(hasOwn(props,key)) {
+      } else if (hasOwn(props, key)) {
         console.warn("props are readonly")
       }
       return true
     }
   })
-  if(setup) {
+  if (setup) {
     const setupContext = createSetupContext(instance)
-    let setupResult = setup(instance.props,setupContext)
-    if(isFunction(setupResult)) {
+    let setupResult = setup(instance.props, setupContext)
+    if (isFunction(setupResult)) {
       instance.render = setupResult //如果setup返回的是函数，那么就是render函数
-    }else if(isObject(setupResult)) {
+    } else if (isObject(setupResult)) {
       instance.setupState = setupResult
     }
   }
-  if(!instance.render){
+  if (!instance.render) {
     // 如果没有render，而写的是template,则要模板编译
     instance.render = Component.render
   }
 }
 export function setupComponent(instance) {
-  const {props,children} = instance.vnode
+  const { props, children } = instance.vnode
   // 组件的属性初始化
-  initProps(instance,props)
+  initProps(instance, props)
   // 插槽的初始化
 
   setupStatefullComponent(instance)
@@ -123,95 +123,95 @@ export function setupComponent(instance) {
 //创建虚拟dom 
 export function createRenderer(renderOptions) {
   let {
-    insert:hostInsert,
-    remove:hostRemove,
-    setElementText:hostSetElementText,
-    setText:hostSetText,
-    parentNode:hostParentNode,
-    nextSibling:hostNextSibling,
-    createElement:hostCreateElement,
-    createText:hostCreateText,
-    patchProps:hostPatchProps
+    insert: hostInsert,
+    remove: hostRemove,
+    setElementText: hostSetElementText,
+    setText: hostSetText,
+    parentNode: hostParentNode,
+    nextSibling: hostNextSibling,
+    createElement: hostCreateElement,
+    createText: hostCreateText,
+    patchProps: hostPatchProps
   } = renderOptions
   function normalize(child) {
-    if(isString(child)){
-      return createVnode(Text,null,child)
+    if (isString(child)) {
+      return createVnode(Text, null, child)
     }
     return child
   }
-  function mountChildren(children,container){
-    for(let i = 0; i < children.length; i++){
+  function mountChildren(children, container) {
+    for (let i = 0; i < children.length; i++) {
       let child = normalize(children[i])
-      patch(null,child,container)
+      patch(null, child, container)
     }
   }
-  function setupRenderEffect(initialVNode,instance,container) {
+  function setupRenderEffect(initialVNode, instance, container) {
     // 创建渲染effect
     // 核心就是调用render，数据变化就重新调用render
-    const componentUpdateFn = () =>{
-      let {proxy} = instance // render中的参数
-      if(!instance.isMounted){
-        const subTree = instance.subTree = instance.render.call(proxy,proxy)
+    const componentUpdateFn = () => {
+      let { proxy } = instance // render中的参数
+      if (!instance.isMounted) {
+        const subTree = instance.subTree = instance.render.call(proxy, proxy)
         // 组件的初始化流程
-        patch(null,subTree,container) //稍后渲染完subTree 会生成真实节点之后挂载
+        patch(null, subTree, container) //稍后渲染完subTree 会生成真实节点之后挂载
         instance.isMounted = true
-      }else {
+      } else {
         // 组件更新流程,做diff算法，比较前后两棵树
         const prevTree = instance.subTree
-        const nextTree = instance.render.call(proxy,proxy)
-        patch(prevTree,nextTree,container)
+        const nextTree = instance.render.call(proxy, proxy)
+        patch(prevTree, nextTree, container)
       }
     }
-    const effect = new ReactiveEffect(componentUpdateFn,()=>{})
+    const effect = new ReactiveEffect(componentUpdateFn, () => { })
     const update = effect.run.bind(effect)
     update()
   }
-  function mountComponent(initialVNode,container){
+  function mountComponent(initialVNode, container) {
     // 1.给组件创造一个实例
     const instance = initialVNode.component = createComponentInstance(initialVNode)
     // 2.需要给组件的实例进行赋值操作
     setupComponent(instance)
     // 3.调用render方法实现组件的渲染逻辑，如果依赖发生变化，组件要重新渲染
-    setupRenderEffect(initialVNode,instance,container)
+    setupRenderEffect(initialVNode, instance, container)
   }
-  function mountElement(vnode,container,anchor) {
-    let {type, props,children,shapeFlag} = vnode
+  function mountElement(vnode, container, anchor) {
+    let { type, props, children, shapeFlag } = vnode
     let el = vnode.el = hostCreateElement(type)  //将真实元素挂载到这个虚拟节点上
-    if(props){
-      for(let key in props){
-        hostPatchProps(el,key,null,props[key])
+    if (props) {
+      for (let key in props) {
+        hostPatchProps(el, key, null, props[key])
       }
     }
-    if(shapeFlag && ShapeFlags.TEXT_CHILDREN){
-      hostSetElementText(el,children)
-    }else if(shapeFlag && ShapeFlags.ARRAY_CHILDREN){
+    if (shapeFlag && ShapeFlags.TEXT_CHILDREN) {
+      hostSetElementText(el, children)
+    } else if (shapeFlag && ShapeFlags.ARRAY_CHILDREN) {
       // 如果有儿子并且是数组的话
-      mountChildren(children,el)
+      mountChildren(children, el)
     }
-    hostInsert(el,container,anchor)
+    hostInsert(el, container, anchor)
   }
-  const processText = (n1,n2,container) => {
-    if(n1 == null){
-      hostInsert((n2.el = hostCreateText(n2.children)),container)
-    }else {
+  const processText = (n1, n2, container) => {
+    if (n1 == null) {
+      hostInsert((n2.el = hostCreateText(n2.children)), container)
+    } else {
       const el = n2.el = n1.el
-      if(n1.children !== n2.children){
-        hostSetText(el,n2.children)
+      if (n1.children !== n2.children) {
+        hostSetText(el, n2.children)
       }
     }
   }
-  const patchProps = (oldProps,newProps,el) =>{
-    for(let key in newProps){
-      hostPatchProps(el,key,oldProps[key],newProps[key])
+  const patchProps = (oldProps, newProps, el) => {
+    for (let key in newProps) {
+      hostPatchProps(el, key, oldProps[key], newProps[key])
     }
-    for(let key in oldProps){
-      if(newProps[key] === null){
-        hostPatchProps(el,key,oldProps[key],null)
+    for (let key in oldProps) {
+      if (newProps[key] === null) {
+        hostPatchProps(el, key, oldProps[key], null)
       }
     }
   }
   const unmountChildren = (children) => {
-    for(let i = 0; i < children.length; i++){
+    for (let i = 0; i < children.length; i++) {
       unmount(children[i])
     }
   }
@@ -219,197 +219,201 @@ export function createRenderer(renderOptions) {
    *h('div',{},[ h('li',{key:'a'},'a'),h('li',{key:'b'},'b')])
     h('div',{},[ h('li',{key:'b'},'b'),h('li',{key:'c'},'c')])
    */
-  const patchKeyedChildren = (c1,c2,container) =>{
+  const patchKeyedChildren = (c1, c2, container) => {
     let e1 = c1.length - 1
     let e2 = c2.length - 1
-    let i  = 0 // 指针
-    // sync from start  1,2,4     1,2,3
-    while(i < e1 && i < e2){
+    let i = 0 // 指针
+    // sync from start  1,2,3    1,2,      [i = 2,e1 = 2,e2 = 1]
+    // sync from start  1,2      1,2,3     [i = 2,e1 = 1,e2 = 2]
+    while (i <= e1 && i <= e2) {
       const n1 = c1[i]
       const n2 = c2[i]
-      if(isSameVnode(n1,n2)){ //两个节点是相同节点，则需要比较孩子和自身的属性
-        patch(n1,n2,container)
-      }else {
+      if (isSameVnode(n1, n2)) { //两个节点是相同节点，则需要比较孩子和自身的属性
+        patch(n1, n2, container)
+      } else {
         break
       }
       i++
     }
-    // sync from end  1,2,3    2,3
-    while(i < e1 && i < e2){
+    // sync from end  1,2,3    2,3          [i = 0,e1 = 0,e2 = -1]
+    // sync from end  2,3      1,2,3        [i = 0,e1 = -1,e2 = 0]
+    while (i <= e1 && i <= e2) {
       const n1 = c1[e1]
       const n2 = c2[e2]
-      if(isSameVnode(n1,n2)){ //两个节点是相同节点，则需要比较孩子和自身的属性
-        patch(n1,n2,container)
-      }else {
+      if (isSameVnode(n1, n2)) { //两个节点是相同节点，则需要比较孩子和自身的属性
+        patch(n1, n2, container)
+      } else {
         break
       }
       e1--
       e2--
     }
-    // [0,1] [0,1,2]  [1,2]=>[0,1,2]
-    if(i > e1){
-      if(i <= e2 ){
+    // sync from start  1,2    1,2,3     [i = 2,e1 = 1,e2 = 2]
+    // sync from end    2,3    1,2,3     [i = 0,e1 = -1,e2 = 0]
+    if (i > e1) {
+      // 新的比老的多
+      if (i <= e2) {
         // 新增元素
         const nextPos = e2 + 1
         // 如果有值说明是在头部追加，如果没有说明是在尾部追加
-        const anchor  = nextPos < c2.length ? c2[nextPos] : null
+        const anchor = c2[nextPos] ? c2[nextPos] : null
         while (i <= e2) {
-          patch(null,c2[i],container,anchor) //插入需要参照物
+          patch(null, c2[i], container, anchor) //插入需要参照物
           i++
         }
       }
-    }else if(i > e2){
-      // 老组件多，新组件少，删除多余老的
+    } else if (i > e2) {
+      // 新的比老的少，删除多余老的
+      // sync from start  1,2,3    1,2,      [i = 2,e1 = 2,e2 = 1]
+      // sync from end    1,2,3    2,3       [i = 0,e1 = 0,e2 = -1]
       while (i <= e1) {
         unmount(c1[i])
         i++
       }
     }
-    // abcdefg abecdhfg i = 2, e1 = 4, e2 = 5
+    // 乱序的情况 c，d，e     e，c，d，h
     const s1 = i
     const s2 = i
-    // 根据新的节点 创造一个映射表，用老的列表项去新列表里面找，有则复用老的，没有删除老item，新的多余的增加
+    // 用新节点创建映射表，用老列表项在新映射表中查找，有复用老的，没有删除老的，新的多余的增加
     const keyToNewIndexMap = new Map()
-    for(let i = s2; i <= e2; i++){
+    for (let i = s2; i <= e2; i++) {
       const child = c2[i]
-      keyToNewIndexMap.set(child.key,i)
+      keyToNewIndexMap.set(child.key, i)
     }
-    const toBepatched = e2 - s2 +1    // [e,c,d,h]
+    const toBepatched = e2 - s2 + 1    // [e,c,d,h]
     const newIndexToOldMapIndex = new Array(toBepatched).fill(0)   //最长递增子序列
     // 拿老的去新的中查找
-    for(let i = s1; i <= e1; i++){
+    for (let i = s1; i <= e1; i++) {  //c，d，e
       const prevChild = c1[i]
       const newIndex = keyToNewIndexMap.get(prevChild.key)
-      if(newIndex === undefined){
+      if (newIndex === undefined) {
         unmount(prevChild)
-      }else {
-        // newIndex-s2 为老项在新项目中存在的情况下，填到newIndexToOldMapIndex的位置 newIndexToOldMapIndex = [5,3,4,0]0代表h为新增元素
-        newIndexToOldMapIndex[newIndex-s2] = i +1 //保证填的肯定不是0
-        // 比较两节点
-        patch(prevChild,c2[newIndex],container)
+      } else {
+        // 将老数组[c,d,e]的位置+1 映射到[e=0,c=0,d=0,h=0]的数组中 =>[3,1,2,0]
+        newIndexToOldMapIndex[newIndex - s2] = i + 1 //保证填的肯定不是0
+        // 比较两节点 patchAttr patchChildren
+        patch(prevChild, c2[newIndex], container)
       }
     }
     let queue = getSequence(newIndexToOldMapIndex)  //[c,d]
     let j = queue.length - 1
-    for(let i = toBepatched - 1; i >=0; i--){
+    for (let i = toBepatched - 1; i >= 0; i--) {
       let lastIndex = s2 + i
       let lastChild = c2[lastIndex]
-      let anchor = lastIndex + 1 < c2.length? c2[lastIndex + 1].el:null
-      if(newIndexToOldMapIndex[i] === 0){
-        patch(null,lastChild,container,anchor)
-      }else{
+      let anchor = lastIndex + 1 < c2.length ? c2[lastIndex + 1].el : null
+      if (newIndexToOldMapIndex[i] === 0) {
+        patch(null, lastChild, container, anchor)
+      } else {
         // 性能消耗，最长递增子序列，减少dom的插入操作
-        if(i !== queue[j]){
-          hostInsert(lastChild.el,container,anchor) //将列表倒叙插入
-        }else{    
+        if (i !== queue[j]) {
+          hostInsert(lastChild.el, container, anchor) //将列表倒叙插入
+        } else {
           j--   //元素不需要移动[a,b,c,d,e,q,f,g] => [a,b,e,c,d,h,f,g]
         }
       }
     }
   }
-  const patchChildren = (n1,n2,el) =>{
+  const patchChildren = (n1, n2, el) => {
     // 比较两个虚拟节点的儿子的差异，el就是当前的父节点
     const c1 = n1.children
     const c2 = n2.children
     // 比较两个儿子列表的差异
     /** 
-     * 1. 之前是数组，现在是文本，删除老节点，用新文本替换
-     * 2. 之前是数组，现在是数组，比较两个数组的差异diff
-     * 3. 之前是文本，现在是空，直接删除老的
-     * 4. 之前是文本，现在是文本，直接更新文本
-     * 5. 之前是文本，现在是数组，删除文本新增儿子
-     * 6. 之前是空，现在是文本
+    1. 都是文本
+    2. 老的是数组，新的是文本
+    3. 老的是文本，新的是数组
+    4. 新老都是数组
+    */
+    /* 
+     1.新的有儿子，旧的没有儿子
+     2.旧的有儿子，新的没有儿子
+     3.新旧儿子都是文本
+     4.新旧儿子都是数组
     */
     const prevShapeFlag = n1.shapeFlag
     const shapeFlag = n2.shapeFlag
-    // 现在是文本
-    if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+    // 现在是文本1，2两种情况合并
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       // 1. 之前是数组
-      if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         unmountChildren(c1)
       }
-      // 4. 之前是文本
-      if(c1 !== c2){
-        hostSetElementText(el,c2)
-      }
-    }else{
-      // 之前是数组
-      if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
-        // 2. 现在也是数组
-        if(shapeFlag & ShapeFlags.ARRAY_CHILDREN){
-          patchKeyedChildren(c1,c2,el)
-        }else{
-          // 现在是空
+      hostSetElementText(el, c2)
+
+    } else {
+      // 现在是数组，但是存在数组为空的情况
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        // 1. 老的是数组 
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          patchKeyedChildren(c1, c2, el)
+        } else {
+          // 新数组有可能为空
           unmountChildren(c1)
         }
-      }else{
+      } else {
         // 之前是文本
-        if(prevShapeFlag & ShapeFlags.TEXT_CHILDREN){
-          hostSetElementText(el,'')
-        }
-        if(shapeFlag & ShapeFlags.ARRAY_CHILDREN){
-          mountChildren(c2,el)
-        }
+        hostSetElementText(el, '')
+        mountChildren(c2, el)
       }
     }
   }
-  const patchElement = (n1,n2) =>{  //先复用 在比较属性 在比较儿子
+  const patchElement = (n1, n2) => {  //先复用 在比较属性 在比较儿子
     let el = n2.el = n1.el
     let oldProps = n1.props || {}
     let newProps = n2.props || {}
-    patchProps(oldProps,newProps,el)
-    patchChildren(n1,n2,el)
+    patchProps(oldProps, newProps, el)
+    patchChildren(n1, n2, el)
   }
-  const processElement = (n1, n2, container,anchor) =>{
-    if(n1 === null) {
+  const processElement = (n1, n2, container, anchor) => {
+    if (n1 === null) {
       // 组件的初始化
-      mountElement(n2,container,anchor)
-    }else {
+      mountElement(n2, container, anchor)
+    } else {
       // 组件的更新
-      patchElement(n1,n2) 
+      patchElement(n1, n2)
     }
   }
-  const processComponent = (n1, n2, container,anchor = null) =>{
-    if(n1 === null) {
+  const processComponent = (n1, n2, container, anchor = null) => {
+    if (n1 === null) {
       // 组件的初始化
-      mountComponent(n2,container)
-    }else {
+      mountComponent(n2, container)
+    } else {
       // 组件的更新
     }
   }
-  const patch = (n1,n2,container,anchor = null) => {
-    if(n1 == n2) return
-    if(n1 && !isSameVnode(n1,n2)){
+  const patch = (n1, n2, container, anchor = null) => {
+    if (n1 == n2) return
+    if (n1 && !isSameVnode(n1, n2)) {
       unmount(n1)
       n1 = null
-    } 
+    }
     const { type, shapeFlag } = n2
     switch (type) {
       case Text:
-        processText(n1,n2,container)
+        processText(n1, n2, container)
         break;
-    
+
       default:
-        if(shapeFlag & ShapeFlags.ELEMENT){
-          processElement(n1,n2,container,anchor)
-        }else if(shapeFlag & ShapeFlags.COMPONENT){
-          processComponent(n1,n2,container)
+        if (shapeFlag & ShapeFlags.ELEMENT) {
+          processElement(n1, n2, container, anchor)
+        } else if (shapeFlag & ShapeFlags.COMPONENT) {
+          processComponent(n1, n2, container)
         }
         break;
     }
   }
-  const unmount = (vnode) =>{
+  const unmount = (vnode) => {
     hostRemove(vnode.el)
   }
   // 这个函数代表两种可能性，一个是dom挂载到容器中，一个是删除容器的内容
-  const render = (vnode,container) => {
-    if(vnode === null){
-      if(container._vnode){
+  const render = (vnode, container) => {
+    if (vnode === null) {
+      if (container._vnode) {
         unmount(container._vnode)
       }
-    }else{
+    } else {
       // 这里既有初始化的逻辑 又有更新的逻辑
-      patch(container._vnode || null,vnode,container) 
+      patch(container._vnode || null, vnode, container)
     }
     container._vnode = vnode
   }
--- packages/runtime-dom/src/modules/event.ts
+++ packages/runtime-dom/src/modules/event.ts
@@ -4,26 +4,34 @@
  * 第二次绑定click事件 'b'
  * 使用createInvoker的话不需要进行el.removeEventListener el.addEventListener
  * 同样都是绑定click 只需要把绑定的event换一下好了
+ * {
+ *  click:funciton(v){
+ *    const invoker = (e)=>invoker.value(e)
+ *    invoker.value = () => alert("123")
+ *    return invoker
+ *  } 
+ * }
+ * 
  */
 function createInvoker(v) {
   const invoker = (e) => invoker.value(e)
   invoker.value = v
   return invoker
 }
-export function patchEvent(el,eventName,nextValue) {
+export function patchEvent(el, eventName, nextValue) {
   // 先移除事件 在重新绑定 性能低 
-  let invokers = el._vei|| (el._vei = {})
+  let invokers = el._vei || (el._vei = {})
   let exits = invokers[eventName]
-  if(exits && nextValue){
+  if (exits && nextValue) {
     exits.value = nextValue
-  }else {
+  } else {
     let event = eventName.slice(2).toLowerCase()   //onClick => click
-    if(nextValue){
+    if (nextValue) {
       const invoker = invokers[eventName] = createInvoker(nextValue)
-      el.addEventListener(event,invoker)
-    }else if(exits){
-      el.removeEventListener(event,exits)
+      el.addEventListener(event, invoker)
+    } else if (exits) {
+      el.removeEventListener(event, exits)
       invokers[eventName] = undefined
     }
-  } 
+  }
 }
